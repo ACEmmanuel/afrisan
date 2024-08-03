21721f09cb6b92d617267196fd8be6698355c0b5
ACEmmanuel <ahanotuemmanuel7@gmail.com>
2024-08-03 09:23:08 -0700

    added transitions and revomed bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,143 +1,259 @@
-import React, { useState, useEffect } from 'react';
-
+import React, {useEffect, useRef, useState} from 'react'
 
 const Header = () => {
-  // const [isScrolled, setIsScrolled] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const toggleMenu = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState({account: false, help: false});
+  const [isDropdownOpenMobile, setIsDropdownOpenMobile] = useState({account: false, help: false});
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  // Larger Screens
+  const accountRef = useRef();
+  const helpRef = useRef();
+  const menuRef = useRef();
+
+  // Mobile Screen
+  const mobileAccountRef = useRef();
+  const mobileHelpRef = useRef();
+
+  const w = 767;
+  
+  // PC FUNCTION
+  const dropDown = (drop) => {
+    setIsDropdownOpen((prev)=>{
+      return {...prev, 
+        [drop]: !prev[drop]
+      }
+    })
+  }
+  
+  // MOBILE FUNCTION
+  const dropDownMobile = (drop) => {
+    setIsDropdownOpenMobile((prev)=>{
+      return {...prev, 
+        [drop]: !prev[drop]
+      }
+    })
+  }
+
+  const openMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-    document.body.style.overflow = isMenuOpen ? 'auto' : 'hidden'; // Prevent scrolling when menu is open
-  };
+    document.body.style.overflow = isMenuOpen ? 'auto' : 'hidden';
+  }
 
   const closeMenu = () => {
-    setIsMenuOpen(false);
-    document.body.style.overflow = 'auto'; // Re-enable scrolling when menu is closed
-  };
+    setIsMenuOpen(!isMenuOpen);
+    document.body.style.overflow = 'auto';
+    
+  }
 
+  
+  
 
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const offset = window.scrollY;
-  //     if (offset > 50) {
-  //       setIsScrolled(true);
-  //     } else {
-  //       setIsScrolled(false);
-  //     }
-  //   };
+  useEffect(()=>{
+    const handleClickOustside = (event) => {
+      if (accountRef.current && !accountRef.current.contains(event.target)){
+        setIsDropdownOpen((prev) => ({ ...prev, account: false }));
+      }
 
-  //   window.addEventListener('scroll', handleScroll);
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, []);
+      if (helpRef.current && !helpRef.current.contains(event.target)){
+        setIsDropdownOpen((prev) => ({ ...prev, help: false }));
+      }
+
+      // if (mobileAccountRef.current && !mobileAccountRef.current.contains(event.target)){
+      //   setIsDropdownOpenMobile((prev) => ({ ...prev, account: false }));
+      // }
+
+      // if (mobileHelpRef.current && !mobileHelpRef.current.contains(event.target)){
+      //   setIsDropdownOpenMobile((prev) => ({ ...prev, help: false }));
+      // }
+
+      if (menuRef.current && !menuRef.current.contains(event.target)){
+        setIsMenuOpen(false);
+        document.body.style.overflow = 'auto';
+      }
+    }
+
+    const handleResize = () => {
+      if (window.innerWidth >= w) {
+        setIsMenuOpen(false);
+        document.body.style.overflow = 'auto';
+        document.body.style.transitionDuration = '100ms';
+      } 
+    };
+  
+    window.addEventListener('resize', handleResize); 
+    handleResize();
+
+    document.addEventListener("mousedown", handleClickOustside);
+
+    return(()=> {
+      document.removeEventListener('resize', handleResize)
+      document.removeEventListener('mousedown', handleClickOustside)
+    })
+
+  
+  }, [])
+
   
   return (
     <>
-    {/* Desktop Menu */}
+        <header className='font-IBM bg-orange-500 p-6 relative text-slate-100 mb-10'>
+          <div className='mx-auto flex justify-between items-center gap-5'>
 
-      {/* <header className={` w-full top-0 left-0 p-6 font-radio transition-all duration-300 flex justify-between items-center h-20 ${isScrolled ? 'transparent text-white' : 'bg-transparent text-black'}`}> */}
-      <header className='w-full top-0 left-0 p-8 font-radio transition-all duration-300 flex justify-between items-center h-20 transparent text-black'>
+            {/* Logo */}
+            <div className="text-2xl font-bold">Afrisan</div>
 
-        <h1 className='text-black font-medium text-2xl'>Afrisan</h1>
-
-
-        {/*Search Section Large Screen*/}
-        <div className='hidden lg:flex'>   
-        {/* md:order-2 */}
-          <form class="max-w-md mx-auto">   
-              <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-              <div class="relative ">
-
-                {/* Search Icon */}
-                  <div class="absolute inset-y-0 start-0 flex items-center ps-4 pointer-events-none ml-16">
-                      <i class='bx bx-search-alt-2 text-[1.25rem] text-gray-500'></i>
-                  </div>
-
-                {/* Search Bar */}
-                  <input type="text" id="default-search" class="block w-[25rem] p-[.9rem] ps-12 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 ml-16 outline-none" placeholder="Search fashion, food..." required />
-                  <button type="submit" class="text-white absolute -end-1 bottom-2 bg-blue-700 hover:bg-blue-800  font-medium rounded-full text-sm px-4 py-2 ">Search</button>
-              </div>
-          </form>
-        </div>
-
-
-        {/* Navigation Section */}
-        <div class="hidden md:flex gap-10 font-DM font-medium text-[15px]">
-          <a href="#" class="text-gray-800 hover:text-blue-600">Home</a>
-
-
-          <div class="group">
-            <button class="flex items-center text-gray-800 hover:text-blue-600">Store
-            <i class='bx bx-chevron-down'></i>
-              </button>
-                
-              <div class="absolute mt-2 w-48 bg-white border border-gray-200 rounded shadow-lg hidden group-hover:block p-2">
-                <a href="#" class="block px-4 py-2 text-gray-900 hover:bg-gray-100">Category 1</a>
-                <a href="#" class="block px-4 py-2 text-gray-900 hover:bg-gray-100">Category 2</a>
-                <a href="#" class="block px-4 py-2 text-gray-900 hover:bg-gray-100">Category 3</a>
-              </div>
-          </div>
-
-          <a href="#" class="text-gray-800 hover:text-blue-600">Contact us</a>
-        </div>
-
-
-        
-
-
-
-
-        {/* Mobile Menu Toggle Button  */}
-        <div className='flex items-center lg:hidden gap-4 text-[1.6rem]'>
-
-          {/* Search Icon and Menu */}
-          <i class='bx bx-search-alt'></i> 
-          {isMenuOpen ? <i class='bx bx-x ' onClick={closeMenu}></i> :   <i class='bx bx-menu' onClick={toggleMenu}></i>}
-        
-
-          {/* Mobile Menu Dropdown*/}
-          {isMenuOpen && (
-            <div className="absolute inset-0 top-[5rem] text-[16px] bg-black z-50 p-6 flex flex-col gap-4 h-max w-[96%] mx-auto rounded-md">
-              <a href="#" className="block px-4 py-2 text-gray-300 hover:bg-blue-600 hover:text-white">Home</a>
-
-              <button className=" flex items-center justify-between w-full px-4 py-2 text-gray-300 hover:bg-blue-600"
-                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-              >
-                Store <i class='bx bx-chevron-down'></i>
-              </button>
-
-
-              {/* Dropdown Menu for Store Mobile Screen*/}
-              {isDropdownOpen && (
-                <div className="" >
-                  <a href="#" className="block px-4 py-2 text-gray-300 hover:bg-blue-600" >Category 1</a>
-                  <a href="#" className="block px-4 py-2 text-gray-300 hover:bg-blue-600">Category 2</a>
-                  <a href="#" className="block px-4 py-2 text-gray-300 hover:bg-blue-600">Category 3</a>
+            {/* Search Bar */}
+            <div className="hidden md:flex flex-1 space-x-5 items-center px-[4rem] relative overflow-hidden">
+              <div className='flex-grow rounded'>
+                <input 
+                type="text" 
+                placeholder="Search..." 
+                className="bg-slate-100 text-black w-full p-3 px-[1rem] outline-none"
+              />
+                <div className='absolute top-0 right-[4rem] bg-black p-5 h-[3rem] grid place-content-center'>
+                  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-4.35-4.35m1.29-5.16A7.5 7.5 0 1011.5 19 7.5 7.5 0 0018 12.5z"></path>
+                  </svg>
                 </div>
-              )}
-              <a href="#" className="block px-4 py-2 text-gray-300 hover:bg-blue-600 hover:text-white">Contact us</a>
+              </div>
             </div>
-        )}
-      </div>
 
 
+            <div className='gap-5 hidden md:flex'>
+                {/* Account */}
+              <div className='relative' onClick={()=>dropDown('account')} ref={accountRef}>
+                <button className='flex items-center gap-1'>
+                  <span>Account</span>
+                  <svg className={`w-4 h-4 transition-transform transform ${isDropdownOpen.account ? 'rotate-180' : ''}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+                  </svg>
+                </button>
+                {isDropdownOpen.account && (
+                 <div className="absolute right-0 mt-2 w-48 bg-white text-black rounded shadow-lg py-2 transition-opacity duration-1000 opacity-100">
+                  <a href="#" className="block py-2 hover:bg-gray-600 px-4">Profile</a>
+                  <a href="#" className="block py-2 hover:bg-gray-600 px-4">Settings</a>
+                  <a href="#" className="block py-2 hover:bg-gray-600 px-4">Logout</a>
+                </div>
+                )}
+              </div>
+
+              {/* Help */}
+              <div className='relative' onClick={()=>{dropDown('help')}} ref={helpRef}>
+                <button className='flex items-center gap-1'>
+                  <span>Help</span>
+                  <svg className={`w-4 h-4 transition-transform transform ${isDropdownOpen.help ? 'rotate-180' : ''}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+                  </svg>
+                </button>
+                {isDropdownOpen.help && (
+                 <div className="absolute right-0 mt-2 w-48 bg-white text-black rounded shadow-lg py-2 transition-opacity duration-300 opacity-100">
+                 <a href="#" className="block px-4 py-2 hover:hover:bg-gray-600">FAQ</a>
+                 <a href="#" className="block px-4 py-2 hover:hover:bg-gray-600">Contact Support</a>
+               </div>
+                )}
+              </div>
+            </div>
+           
 
 
+            {/* Mobile Icon with Cart Icon and Slide in Menu box*/}
+            <div className='flex gap-5'>
+              <div className={`text-white font-bold`}>
+                {/* Cart Icon*/}
+                <svg className='size-[1.4rem]' fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                  <path d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
+                </svg>
+              </div>
+              
+              {/* Menu Icon */}
+              <div className="md:hidden flex items-center" >
+                <button className="text-white" onClick={()=>openMenu()}>
+                  <svg className={`w-6 h-6 transition-transform transform ${isMenuOpen? 'rotate-90' : ''} `} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+                  </svg>
+                </button>
+              </div>
+              
+              {/* Actual Menu Slide-in Box*/}
+              <div className={`fixed top-0 right-0 w-[85vw] h-full bg-slate-600 transform transition-transform duration-[400ms]  ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} p-5 shadow-lg`} ref={menuRef}>
+                
+                {/*Close Button */}
+                <span className='font-bold cursor-pointer flex items-center gap-2' onClick={()=>closeMenu()}>
+                <svg className={`size-[1rem]`} fill='white' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/>
+                </svg>
+                <span className='uppercase'>close</span>
+                </span>
 
-      </header>
+
+                {/* Account and Menu Section */}
+                <div className='flex flex-col gap-5 mt-14'>
+                  {/* Search Bar */}
+                  <div className="items-center relative overflow-hidden rounded">
+                    <div className='flex-grow'>
+                      <input 
+                      type="text" 
+                      placeholder="Search..." 
+                      className="bg-white text-white w-full p-3 outline-none"
+                      />
+                      <div className='absolute top-0 right-0 bg-orange-500 p-5 h-[3rem] grid place-content-center'>
+                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-4.35-4.35m1.29-5.16A7.5 7.5 0 1011.5 19 7.5 7.5 0 0018 12.5z"></path>
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                  {/* Account */}
+                    <div className='relative text-[20px] font-bold mt-5' onClick={()=>dropDownMobile('account')} ref={mobileAccountRef}>
+                      <button className='flex items-center gap-1'>
+                        <span>Account</span>
+                        <svg className={`w-4 h-4 transition-transform transform ${isDropdownOpenMobile.account ? 'rotate-180' : ''}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+                        </svg>
+                      </button>
+                      {isDropdownOpenMobile.account && (
+                      <div className="relative right-0 mt-0 w-full rounded py-2 transition-opacity duration-1000 opacity-100 text-[18px] font-medium text-slate-100">
+                        <a href="#" className="block py-2 hover:bg-gray-600 px-4">Profile</a>
+                        <a href="#" className="block py-2 hover:bg-gray-600 px-4">Settings</a>
+                        <a href="#" className="block py-2 hover:bg-gray-600 px-4">Logout</a>
+                      </div>
+                      )}
+                    </div>
+
+                    {/* Help */}
+                    <div className='relative text-[20px] font-bold' onClick={()=>{dropDownMobile('help')}} ref={mobileHelpRef}>
+                      <button className='flex items-center gap-1'>
+                        <span>Help</span>
+                        <svg className={`w-4 h-4 transition-transform transform ${isDropdownOpenMobile.help ? 'rotate-180' : ''}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+                        </svg>
+                      </button>
+                      {isDropdownOpenMobile.help && (
+                      <div className="relative right-0 mt-2 w-full rounded py-2 transition-opacity duration-300 opacity-100 text-[18px] font-medium text-slate-100">
+                      <a href="#" className="block px-4 py-2 hover:hover:bg-gray-600">FAQ</a>
+                      <a href="#" className="block px-4 py-2 hover:hover:bg-gray-600">Contact Support</a>
+                    </div>
+                      )}
+                    </div>
+
+                </div>
+                
+                
+
+              
+
+
+              </div>   {/* Actual Menu Slide-in Box CLOSE*/}   
+            </div> {/* Mobile Icon with Cart Icon and Slide in Menu box CLOSE*/}
+
+          </div>
+        </header>
+
+        
+        
     </>
-  );
-};
-
+  )
+}
 
 export default Header;
-
-
-
-// #EA661E
-
-
-
-     
