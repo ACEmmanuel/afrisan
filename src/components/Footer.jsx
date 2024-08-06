8832a984f47207ba50985818468c487c7867de73
ACEmmanuel <ahanotuemmanuel7@gmail.com>
2024-08-06 05:45:35 -0700

    redid the footer component, giving it a brand new look

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,42 +3,142 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-        <div className=' bg-black text-white font-DM text-nowrap cursor-pointer mt-[100vh]'>
-            <div className=' container mx-auto px-2 py-6 text-center space-y-10 divide-y divide-gray-500'>
-                
-                <div className='justify-between items-center space-y-10 md:space-y-0 md:flex'>
-                    <div className='flex flex-col gap-3 items-center md:items-start'>
-                        <p className='font-bold text-[1.5rem]'>AFRISAN</p>
-                        <p className='text-xs'>EXPLORE AFRICAN-MADE EXCELLENCE</p>
-                        <p>SOCIALS HERE</p>
-                    </div>
+        <div className='bg-[#1A1C2C]'>
+        <div className='container mx-auto text-white/90 mt-7 font-IBM '>
+        <div className='mx-auto flex flex-col lg:flex-row items-start flex-nowrap gap-8 px-5 py-10 font-medium text-[15px]'>
+            <div className='lg:w-[33%]'>
+                <h1 className='font-medium text-[1.5rem] lg:text-[2rem]'>AFRISAN</h1>
+                <p className='max-w-[40rem] font-normal pr-3 leading-[1.5] text-[13px] lg:text-[14px]'>Reinventing the way of creating websites, we aim to create the most master-paced WordPress theme available on the market.</p>
+            </div>
 
-                    <div className='flex flex-row text-[14px] justify-around items-center md:gap-[6rem] lg:gap-[10rem] capitalize'>
-
-                        <div className='flex flex-col gap-2'>
-                            <p>Terms & conditions</p>
-                            <p>Privacy Policy</p>
-                            <p>Shipping & Payment Info</p>
-                        </div>   
-
-
-                        <div className='flex flex-col gap-2'>
-                            <p>Return Policy</p>
-                            <p>About Us</p>
-                            <p>Report Abuse</p>
-                        </div>   
-                        
-                    </div>
+            <div className='leading-[3] lg:w-[33%]'>
+                <p className='text-lg font-bold'>CONTACT US</p>
+                <p className='contact'>202 Helga Springs Rd, Crawforss, TN 38554</p>
+                <p className='contact'>Call Us: <span>800.275.8777</span></p>
+                <p className='contact'>alex@company.com</p>
+            </div>
+            
+            <div className='space-y-6 lg:w-[33%]'>
+                <p className='text-lg font-bold'>SIGN UP FOR EMAIL UPDATES</p>
+                <div className='relative'>  
+                    <input className='input'/>
+                    <div className='search'>Subscribe</div>
                 </div>
-
-                <div className='text-[.75rem] pt-6'>
-                    <p>copyright 2024 afrisan, All Rights Reserved</p>
-                </div>
-
+                <p>Sign up with your email address to receive news and updates</p>
             </div>
         </div>
+
+        <div className='w-[100%] border-t mx-auto border-slate-700 py-6'>
+            <div className='flex flex-row flex-wrap gap-7 mx-auto justify-between text-[13px] px-3'>
+                <div className='flex flex-row flex-wrap gap-7 lg:order-2 mx-auto md:mx-0 text-center'>
+                    <a href="" className='text-nowrap'>Home</a>
+                    <a href="" className='text-nowrap'>About Us</a>
+                    <a href="" className='text-nowrap'>Our Team</a>
+                    <a href="" className='text-nowrap'>Shop</a>
+                    <a href="" className='text-nowrap'>Blog</a>
+                    <a href="" className='text-nowrap'>Contact Us</a>
+                </div>
+                <div className='mx-auto md:mx-0 lg:order-1 border-t md:border-none md:pt-0 border-slate-700 pt-4 '>
+                    <p className='text-[12px]'>Copyright @ 2023 Afrisan. All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
     </>
   )
 }
 
 export default Footer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+
+// const Footer = () => {
+//   return (
+//     <>
+//         <div className='bg-[#1A1C2C]'>
+//         <div className='container mx-auto text-white/90 mt-7 font-DM '>
+//         <div className='mx-auto flex flex-col lg:flex-row items-start flex-nowrap gap-8 px-5 py-10 font-medium text-[15px]'>
+//             <div className='space-y-6 lg:w-[33%]'>
+//                 <h1 className='font-bold text-[2rem]'>SYZN</h1>
+//                 <p className='max-w-[40rem]'>Reinventing the way of creating websites, we aim to create the most master-paced WordPress theme available on the market.</p>
+//             </div>
+
+//             <div className='space-y-6 lg:w-[33%]'>
+//                 <p className='left-green relative font-bold'>CONTACT US</p>
+//                 <p>202 Helga Springs Rd, Crawforss, TN 38554</p>
+//                 <p>Call Us: <span className='font-bold'>800.275.8777</span></p>
+//                 <p>alex@company.com</p>
+//             </div>
+            
+//             <div className='space-y-6 lg:w-[33%]'>
+//                 <p className='left-green relative font-bold'>SIGN UP FOR EMAIL UPDATES</p>
+//                 <div className='relative'>  
+//                     <input className='input'/>
+//                     <div className='search'>Subscribe</div>
+//                 </div>
+//                 <p>Sign up with your email address to receive news and updates</p>
+//             </div>
+//         </div>
+
+//         <div className='w-[100%] border-t mx-auto border-slate-700 py-6'>
+//             <div className='flex flex-row flex-wrap gap-7 mx-auto justify-between text-[13px] px-3'>
+//                 <div className='flex flex-row flex-wrap gap-7 lg:order-2 mx-auto md:mx-0 text-center'>
+//                     <a href="" className='text-nowrap'>Home</a>
+//                     <a href="" className='text-nowrap'>About Us</a>
+//                     <a href="" className='text-nowrap'>Our Team</a>
+//                     <a href="" className='text-nowrap'>Shop</a>
+//                     <a href="" className='text-nowrap'>Blog</a>
+//                     <a href="" className='text-nowrap'>Contact Us</a>
+//                 </div>
+//                 <div className='mx-auto md:mx-0 lg:order-1 border-t md:border-none md:pt-0 border-slate-700 pt-4 '>
+//                     <p className='text-[12px]'>Copyright @ 2023 Sokka. All rights reserved.</p>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//     </div>
+//     </>
+//   )
+// }
+
+// export default Footer
