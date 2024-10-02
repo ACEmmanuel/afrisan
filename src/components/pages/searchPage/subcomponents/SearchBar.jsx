9c330e07c8eb28481ebfadd0198dce09774fb942
ACEmmanuel <ahanotuemmanuel7@gmail.com>
2024-10-02 04:29:39 -0700

    added X button in the search input and gave it clear function

--- src/components/pages/searchPage/subcomponents/SearchBar.jsx
+++ src/components/pages/searchPage/subcomponents/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 //This header belongs to the search page, i know it looks bad please bear with me
 
 
@@ -6,10 +6,14 @@ import React, { useState } from 'react';
 import { fetchName } from '../../../../api/api';
 
 const SearchBar = ({setSearchResults, setError}) => {
+  
+  const inputRef = useRef(null);
 
-  // const [search, setSearch] = useState('');
-  // const [error, setError] = useState('');
-
+  const clearInput = () => {
+    inputRef.current.value = '';
+    setSearchResults([]);
+  }
+  
   const terms = async (e) => {
     const value = e.target.value;
     // setSearch(value);
@@ -41,9 +45,12 @@ const SearchBar = ({setSearchResults, setError}) => {
           onChange={terms}
           placeholder="Search products, brands and categories"
           className="pl-6 pr-16 w-[95%] font-medium rounded-full bg-white text-gray-700 cursor-text text-[14px] py-[0.9rem] shadow-sm outline outline-1 outline-gray-300 focus:outline-2 focus:outline-gray-400 truncate"
+          ref={inputRef}
         />
 
-        <svg class="w-6 h-6 text-gray-800 absolute right-[1.3rem]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+        <svg class="w-6 h-6 text-gray-800 absolute right-[6%] cursor-pointer" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" 
+        onClick={clearInput}
+        >
           <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z" clip-rule="evenodd"/>
         </svg>
 
